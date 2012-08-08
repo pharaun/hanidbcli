0a4e31d75c3f89f3d3d916c58c64d30c944f0141
Anja Berens <pharaun666@gmail.com>
2012-08-08 00:56:26 -0700

    Documented almost all of the header differences, should be able to use only a few datatype to cover the majority of the headers

--- parsing.hs
+++ parsing.hs
@@ -1,16 +1,75 @@
 -- Basic Anidb parser
 import Text.ParserCombinators.Parsec
 
-
 parseAnidb :: String -> Either ParseError [[String]]
 parseAnidb input = parse anidbReply "(unknown)" input
 
--- Sample format
--- 
--- {three digit return code} {opt session key} {opt ip}:{opt port} {return string}\n
--- {three digit return code} {opt salt} {return string}\n
--- {opt compress (00)}{opt tag }{three digit return code} {str return string}{ - opt error message}\n
+-- Protocol defination
 --
+-- opt - optional
+-- int2 - 2 byte Integer (in string representation)
+-- int4 - 4 byte Integer (in string representation)
+-- boolean - true or false - use '1' for true, '0' for false
+-- str - String (UDP packet length restricts string size to 1400 bytes)
+-- hexstr - a hex representation of a decimal value, two characters per byte.
+--
+-- Header:
+-- {opt compressed int2 (00)}
+-- {opt tag str}
+-- {three digit return code (int3)}
+-- {opt session key (str, 4-6 char)}
+-- {opt str ip}:{opt int2 port}
+-- {opt str salt}
+-- {str return string}
+-- {opt error message str}\n
+--
+-- Sample (assuming all can have compression and tag) header:
+-- 500 LOGIN FAILED 
+--
+-- 504 CLIENT BANNED - {str reason}
+-- 6xx INTERNAL SERVER ERROR - {str errormessage} 
+--
+-- 209 {str salt} ENCRYPTION ENABLED 
+--
+-- 200 {str session_key} LOGIN ACCEPTED
+-- 201 {str session_key} LOGIN ACCEPTED - NEW VERSION AVAILABLE 
+--
+-- With NAT enabled - (ipv4 for now...)
+-- 200 {str session_key} {str ip}:{int2 port} LOGIN ACCEPTED
+-- 201 {str session_key} {str ip}:{int2 port} LOGIN ACCEPTED - blah
+--
+--
+-- Sample headers with known no data section, thus can parse next line:
+-- 555 BANNED
+-- {str reason}
+--
+-- 6xx INTERNAL SERVER ERROR 
+-- ERROR: {str errormessage} 
+--
+-- 300 PONG 
+-- {int4 port} (when nat=1) 
+--
+-- 998 VERSION 
+-- {str server version} 
+--
+-- 208 UPTIME 
+-- {int4 udpserver uptime in milliseconds} 
+--
+-- With imgServer enabled - Context/connection dependent
+-- 200 {str session_key} LOGIN ACCEPTED 
+-- {str image server name}
+--
+-- 201 {str session_key} LOGIN ACCEPTED - NEW VERSION AVAILABLE 
+-- {str image server name} 
+--
+--
+-- Probably unsupported headers:
+-- 253 {int2 start} {int2 end} {int2 total} BUDDY LIST 
+-- 254 {int2 start} {int2 end} {int2 total} BUDDY STATE
+-- 
+--
+--
+-- Data section:
 -- {data field 0}|{data field 1}|...|{data field n} 
 --
 
@@ -24,20 +83,39 @@ anidbReply = do
 
 headers :: GenParser Char st [String]
 headers = do
+    comp <- compression
+    aTag <- tag
     return_code <- returnCode
     char ' '
     return_string <- returnString
     eol
-    return [show return_code, return_string]
+    return [show comp, aTag, show return_code, return_string]
+
+-- For now just return true/false but i would like to "decompress"
+-- the remaining data and then feed it back into the parser
+compression :: GenParser Char st Bool
+compression = 
+    (string "00" >> return True)
+    <|> (return False)
+
+-- For now assume no limitation in length of tag
+-- Need to tweak it so it can accept digits in the string
+-- if i don't exclude digits it will eat up the return code if
+-- the tag does not exist
+tag :: GenParser Char st String
+tag = do
+        a <- many (noneOf " 0123456789")
+        char ' '
+        return a
+    <|> (return "")
 
 returnCode :: GenParser Char st Integer
 returnCode = do 
-    ds <- many1 digit
+    ds <- count 3 digit
     return $ read ds
 
 returnString :: GenParser Char st String
-returnString = do
-    many (noneOf "\n")
+returnString = many (noneOf "\n")
 
 eol :: GenParser Char st Char
 eol = char '\n'
@@ -60,5 +138,4 @@ remainingItem =
     <|> (return [])
 
 itemContent :: GenParser Char st String
-itemContent =
-    many (noneOf "|\n")
+itemContent = many (noneOf "|\n")
