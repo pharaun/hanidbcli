185846cc97876a47fd71502d0030973e3c53e833
Anja Berens <pharaun666@gmail.com>
2012-09-10 02:50:43 -0700

    Lots of alternative experiments, the conduits approach ended up being unusable slow, and other approaches end up space exploding, so i'm working on a new approach, it works but it needs some help with strict evalulation

--- hAnidbCli.hs
+++ hAnidbCli.hs
@@ -1,5 +1,92 @@
-import HClient.Options
-import System.Console.CmdArgs
+--import Prelude hiding (FilePath)
+import Data.Word (Word8)
+import Numeric (showHex)
+
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S8
+
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HMap
+
+--import Filesystem.Path.CurrentOS (FilePath, encodeString, decodeString)
+
+import Data.Conduit (($$), runResourceT)
+import qualified Data.Conduit.List as CL
+import Data.Conduit.Filesystem (sourceFile, traverse)
+import Crypto.Conduit (sinkHash, hashFile)
+
+import Crypto.Hash.Ed2k (ED2K, hashlazy, initEd2k, updateEd2k, finalizeEd2k, Ctx)
+import Data.Serialize (encode)
+
+--import HClient.Options
+--import System.Console.CmdArgs
+--
+--main :: IO ()
+--main = print =<< cmdArgsRun optionMode
+import qualified Data.ByteString.Lazy as BS
+import System.IO
+import Control.Exception (bracket, evaluate)
 
 main :: IO ()
-main = print =<< cmdArgsRun optionMode
+main = do
+    let file = "test.mkv"
+
+    mapM_ (\path -> bracket (openFile path ReadMode) hClose (\fh -> do
+        z <- foreach fh initEd2k
+        --z <- finalizeEd2k $ foldl updateEd2k initEd2k
+        putStrLn $ fileLine path z
+        )) [file]
+
+    where
+        foreach :: Handle -> Ctx -> IO S.ByteString
+        foreach fh ctx = do
+            a <- S.hGet fh (9500*1024)
+            evaluate (finalizeEd2k ctx)
+--            putStrLn $ fileLine "foo" (finalizeEd2k ctx)
+            case S.null a of
+                True  -> return $ finalizeEd2k ctx
+                False -> foreach fh (updateEd2k ctx a)
+
+--    mapM_ (\path -> putStrLn . fileLine path =<< BS.readFile path) [file]
+
+fileLine :: FilePath -> S.ByteString -> String
+fileLine path c = hash c ++ " " ++ path
+
+hash :: S.ByteString -> String
+hash = show . toHex-- . hashlazy
+
+--main :: IO ()
+--main = do
+--    let folder = "./test/a.mkv"
+--
+--    digest <- hashFile folder
+--    let hash = toHex . encode $ (digest :: ED2K)
+--    putStrLn $ show hash
+
+--    md5Map <- buildMap $ decodeString folder
+--    (putStrLn . show) `mapM_` md5Map
+--
+--buildMap :: FilePath -> IO [(S.ByteString, FilePath)]
+--buildMap dir =
+--    traverse False dir $$ CL.foldM addFP []
+--  where
+--    addFP :: [(S.ByteString, FilePath)] -> FilePath -> IO [(S.ByteString, FilePath)]
+--    addFP hmap fp = do
+--        digest <- runResourceT $ sourceFile fp $$ sinkHash
+--        let hash = toHex . encode $ (digest :: ED2K)
+--        putStrLn $ show hash
+--        return $ hmap ++ [(hash, fp)]
+
+-- Overall, this function is pretty inefficient. Writing an optimized version
+-- in terms of unfoldR is left as an exercise to the reader.
+toHex :: S.ByteString -> S.ByteString
+toHex =
+    S.concatMap word8ToHex
+  where
+    word8ToHex :: Word8 -> S.ByteString
+    word8ToHex w = S8.pack $ pad $ showHex w []
+
+    -- We know that the input will always be 1 or 2 characters long.
+    pad :: String -> String
+    pad [x] = ['0', x]
+    pad s   = s
