5bb21fb93c7a3bc1a67c982a42a5576f90db9e8f
Anja Berens <pharaun666@gmail.com>
2012-09-25 01:17:10 -0700

    Rebuilt the tests to better take advantage of arbitrary input and reject non-matching input or modify them

--- quickcheck.hs
+++ quickcheck.hs
@@ -48,17 +48,17 @@ instance Arbitrary (IS.IxSet UniqueFile) where
         files <- listOf1 arbitrary
         return (IS.fromList files)
 
+instance Arbitrary (Set.Set FilePath) where
+    arbitrary = do
+        filePath <- elements ["/home/foo0", "/tmp/bar.tar.gz", "/long/silly/[file]name.exe.jpg"]
+        return (Set.fromList [decodeString filePath])
+
 hexChar :: Gen Char
 hexChar = oneof [ elements ['A'..'F'], elements ['0'..'9'] ]
 
 hexHash :: Gen String
 hexHash = vectorOf 32 hexChar
 
-unknownDeviceUnknownFile :: UniqueFile -> Gen UniqueFile
-unknownDeviceUnknownFile uf = suchThat arbitrary (deviceOnly uf)
-    where
-        deviceOnly :: UniqueFile -> UniqueFile -> Bool
-        deviceOnly (UniqueFile _ a b _ _) (UniqueFile _ x y _ _) = a /= x && b /= y
 
 knownDeviceFile :: UniqueFile -> UniqueFile -> UniqueFile
 knownDeviceFile uf1 uf2 = fileOnly uf1 uf2
@@ -66,14 +66,25 @@ knownDeviceFile uf1 uf2 = fileOnly uf1 uf2
         fileOnly :: UniqueFile -> UniqueFile -> UniqueFile
         fileOnly (UniqueFile _ a b _ _) (UniqueFile x _ _ y z) = UniqueFile x a b y z
 
-knownDeviceUnknownFile :: UniqueFile -> Gen UniqueFile
-knownDeviceUnknownFile uf = matchDevice uf <$> suchThat arbitrary (unMatchFile uf)
-    where
-        unMatchFile :: UniqueFile -> UniqueFile -> Bool
-        unMatchFile (UniqueFile _ a _ _ _) (UniqueFile _ x _ _ _) = a /= x
+-- Merge in a UniqueFile to use the same device id
+mergeDeviceID :: UniqueFile -> UniqueFile -> UniqueFile
+mergeDeviceID (UniqueFile _ _ a _ _) (UniqueFile w x _ y z) = UniqueFile w x a y z
 
-        matchDevice :: UniqueFile -> UniqueFile -> UniqueFile
-        matchDevice (UniqueFile _ _ a _ _) (UniqueFile w x _ y z) = UniqueFile w x a y z
+-- Reject if same device and same file
+sameDevice :: UniqueFile -> UniqueFile -> Bool
+sameDevice (UniqueFile _ _ b _ _) (UniqueFile _ _ y _ _) = b == y
+
+-- Reject if same device and same file
+sameFile :: UniqueFile -> UniqueFile -> Bool
+sameFile (UniqueFile _ a _ _ _) (UniqueFile _ x _ _ _) = a == x
+
+-- Merge in a UniqueFile with a provided Set of FilePaths
+mergedUniqueFile :: UniqueFile -> (Set.Set FilePath) -> UniqueFile
+mergedUniqueFile (UniqueFile a b c d e) x = UniqueFile (Set.union a x) b c d e
+
+-- Replace UniqueFile's FilePath Set with provided one
+newUniqueFile :: UniqueFile -> (Set.Set FilePath) -> UniqueFile
+newUniqueFile (UniqueFile _ b c d e) x = UniqueFile x b c d e
 
 
 -- Properties: (init syncset)
@@ -94,7 +105,11 @@ prop_newFile_SyncSet uf xs = (updateNewFile (initSyncSet xs) uf) == (xs, IS.from
 prop_newFile_sameFile_SyncSet uf =
     (updateNewFile (updateNewFile (initSyncSet IS.empty) uf) uf) == (IS.empty, IS.fromList [uf])
 
---  Adding a "Hardlink" (same file different filename) must result with both file in SyncSet (?)
+--  Adding a "Hardlink" (same file different filename) must result with both file in SyncSet
+prop_newFile_hardLinkFile_SyncSet uf path =
+    let nhlf = mergedUniqueFile uf path
+        uf2  = newUniqueFile uf path
+    in (updateNewFile (updateNewFile (initSyncSet IS.empty) uf) uf2 == (IS.empty, IS.fromList [nhlf]))
 
 
 -- Properties: (isNewFile)
@@ -102,17 +117,18 @@ prop_newFile_sameFile_SyncSet uf =
 prop_isNewFile_Empty_SyncSet uf = (isNewFile (initSyncSet IS.empty) uf) == True
 
 --  if device id does not exist in known set, it must always return true
-prop_isNewFile_unknownDeviceUnknownFile_SyncSet uf = do
-    knduf <- unknownDeviceUnknownFile uf
-    return (isNewFile (initSyncSet $ IS.fromList [knduf]) uf == True)
+prop_isNewFile_unknownDeviceUnknownFile_SyncSet uf1 uf2 =
+    not (sameDevice uf1 uf2) ==>
+        (isNewFile (initSyncSet $ IS.fromList [uf1]) uf2 == True)
 
 --  if device id exist and file id does not exist it must return true
-prop_isNewFile_knownDeviceUnknownFile_SyncSet uf = do
-    kdf <- knownDeviceUnknownFile uf
-    return (isNewFile (initSyncSet $ IS.fromList [kdf]) uf == True)
+prop_isNewFile_knownDeviceUnknownFile_SyncSet uf1 uf2 = -- TODO: probably better as a newtype on deviceID
+    not (sameFile uf1 uf2) ==>
+        let uf3 = mergeDeviceID uf1 uf2
+        in (isNewFile (initSyncSet $ IS.fromList [uf1]) uf3 == True)
 
 --  if device id exists and file id exists it must return false (wrong?)
-prop_isNewFile_knownDeviceKnownFile_SyncSet uf1 uf2 =
+prop_isNewFile_knownDeviceKnownFile_SyncSet uf1 uf2 = -- TODO: probably better as a newtype on fileID
     let kdf = knownDeviceFile uf1 uf2
     in (isNewFile (initSyncSet $ IS.fromList [kdf]) uf1 == False)
 
@@ -128,6 +144,12 @@ prop_isNewFile_knownDeviceKnownFile_SyncSet uf1 uf2 =
 -- Properties: (updating syncset)
 --  a file added MUST always be either removed from known set or added to new set
 
+-- Properties: (hardlink, symlink)
+--  Deal with hardlink and symlink in a safe manner
+--  hardlink -> multiple path assocated with a unique file
+--  symlink -> (? unique file pointer? or some sort of marker to identify as a symlink)
+--      could be done via lstat + stat and using one or the other as the dst vs source
+
 
 -- Test driver and stuff
 -- Borrowed from the XMonad project
@@ -140,6 +162,7 @@ main = do
     quickCheck prop_newFile_Empty_SyncSet
     quickCheck prop_newFile_SyncSet
     quickCheck prop_newFile_sameFile_SyncSet
+    quickCheck prop_newFile_hardLinkFile_SyncSet
 
     quickCheck prop_isNewFile_Empty_SyncSet
     quickCheck prop_isNewFile_unknownDeviceUnknownFile_SyncSet
